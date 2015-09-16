62239f084f2eaf5be6dfca7b7293f4f4bf69efa0
Matteo <matteo.crc@gmail.com>
2015-09-16 14:01:20 +0200

    commit

--- globalvar.m
+++ globalvar.m
@@ -2,6 +2,34 @@ global CAMERA_ANGLES
 global FC
 global CC
 global ALPHA_C
+global shoulderOffsetY
+global elbowOffsetY
+global upperArmLength
+global shoulderOffsetZ
+global LowerArmLength
+global HandOffsetX
+global HandOffsetZ
+global HipOffsetZ
+global HipOffsetY
+global ThighLength
+global TibiaLength
+global FootHeight
+global NeckOffsetZ
+
+shoulderOffsetY = 98;
+elbowOffsetY = 15;
+upperArmLength = 105;
+shoulderOffsetZ = 100;
+HandOffsetX = 57.75;
+HandOffsetZ = 12.31;
+LowerArmLength = 55.95;
+HipOffsetZ = 85; 
+HipOffsetY = 50;
+ThighLength = 100;
+TibiaLength = 102.90;
+FootHeight = 45.11;
+NeckOffsetZ = 126.5;
+
 
 CAMERA_ANGLES=[58.71;0;63.64];
 
