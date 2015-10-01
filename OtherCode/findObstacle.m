ec815dae22a809d560a6b2fd694818417d388ae9
Matteo <matteo.crc@gmail.com>
2015-10-01 13:02:19 +0200

    commit

--- OtherCode/findObstacle.m
+++ OtherCode/findObstacle.m
@@ -1,27 +1,34 @@
-function [cx,cy,ray]=findObstacle(im)
+function [cx,cy,ray]=findObstacle(imBackground, imForeground, threshold)
 
-r=im(:,:,1); 
-g=im(:,:,2); 
-b=im(:,:,3);
-diff=imsubtract(g,rgb2gray(im));
-bw=im2bw(diff,0.03);%0.18
-area=bwareaopen(bw,8000);
-indexes=find(area>0);
-minindexes=min(indexes);
-maxindexes=max(indexes);
+m1 = mean2(rgb2gray(imBackground));
+m2 = mean2(rgb2gray(imForeground));
+newImForeground = imForeground + (m1-m2);
 
-[B,~,~,~]=bwboundaries(area);
+imDiff = rgb2gray(imBackground)-rgb2gray(newImForeground);
 
-boundary = B{1};
+imDiff = abs(imDiff)>threshold;
+[B,~,~,~]=bwboundaries(imDiff);
+
+ind = 0;
+maxSize = 0;
+for i = 1:size(B)
+    if( size(B{i},1)>maxSize)
+        maxSize = size(B{i},1);
+        ind = i;
+    end
+end
+
+boundary = B{ind}
+
 maxcol=max(boundary(:,2));
 mincol=min(boundary(:,2));
 centercol=(maxcol-mincol)/2;
 maxrow=max(boundary(:,1));
 minrow=min(boundary(:,1));
 centerrow=(maxrow-minrow)/2;
-im(minrow+centerrow-5:minrow+centerrow+5,mincol+centercol-5:mincol+centercol+5,:)=255;
-
-imshow(im)
+imForeground(minrow+centerrow-5:minrow+centerrow+5,mincol+centercol-5:mincol+centercol+5,:)=255;
+figure 
+imshow(imForeground)
 cx=mincol+centercol;
 cy=minrow+centerrow;
 ray=max(sqrt((boundary(:,2)-cx).^2+(boundary(:,1)-cy).^2));
@@ -30,19 +37,4 @@ ang=0:0.01:2*pi;
 xp=ray*cos(ang);
 yp=ray*sin(ang);
 plot(cx+xp,cy+yp);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
